b42c9a5786c31c9a1669185d559c8d7833fe854d
DaniilZinoviev06 <daniilzinoviev95@gmail.com>
2024-10-28 23:51:46 +0300

    string to char function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
+
+
+// func to split a string into chars
+void stringToChars(std::string str){
+  std::unordered_map<char, int> char_map;
+  
+  for(char _char : str){
+    char_map[_char]++;
+  }
+  
+  // debug
+  for (const auto& element : char_map) {
+    std::cout << element.first << ": " << element.second << '\n';
+  } 
+
+}
 
 int main(){
+  std::string str = "A watched pot never boils";
   
+  stringToChars(str);
 } 
